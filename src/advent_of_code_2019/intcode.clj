48c4b9d80390964a543eb7bd020a397d0139c8ad
Timothy Andrew <mail@timothyandrew.net>
2019-12-09 13:17:56 +0530

    day 9 part 1

--- src/advent_of_code_2019/intcode.clj
+++ src/advent_of_code_2019/intcode.clj
@@ -4,58 +4,69 @@
             [clojure.core.async :as a]
             [clojure.string :as str]))
 
-(defn read-tape [tape pos mode]
-  (case (or mode :position)
+(defn read-tape [tape pos mode rel-base]
+  (case (or mode :position) 
     :immediate (nth tape pos)
-    :position (nth tape (nth tape pos))))
+    :position (nth tape (nth tape pos))
+    :relative (nth tape (+ rel-base (nth tape pos)))))
 
-(defn op [operator tape pos [lmode rmode]]
-  (let [dest-position (nth tape (+ pos 3))
-        values [(read-tape tape (inc pos) lmode) (read-tape tape (+ pos 2) rmode)]]
-    [4 (assoc tape dest-position (reduce operator values))]))
+(defn dest-pos [tape pos rel-base mode]
+  (case (or mode :position)
+          :position (nth tape pos)
+          :relative (+ rel-base (nth tape pos))))
 
-(defn mov [in tape pos]
-  (let [dest-position (nth tape (inc pos))]
-    [2 (assoc tape dest-position in)]))
+(defn op [operator tape pos [lmode rmode wmode] rel-base]
+  (let [dest-position (dest-pos tape (+ pos 3) rel-base wmode) 
+        values [(read-tape tape (inc pos) lmode rel-base)
+                (read-tape tape (+ pos 2) rmode rel-base)]]
+    [{:jump 4} (assoc tape dest-position (reduce operator values))]))
+
+(defn mov [in tape pos [mode] rel-base]
+  (let [dest-position (dest-pos tape (inc pos) rel-base mode)]
+    [{:jump 2} (assoc tape dest-position in)]))
 
 (defn read-input [{:keys [input]}]
   (let [input (a/<!! input)]
     input))
 
-(defn send-output [tape pos [mode] {:keys [output]}]
-  (let [out (read-tape tape (inc pos) mode)]
+(defn send-output [tape pos [mode] {:keys [output]} rel-base]
+  (let [out (read-tape tape (inc pos) mode rel-base)]
    (a/>!! output out)
-   [2 tape]))
+   [{:jump 2} tape]))
 
-(defn jmp-if-false [tape pos [mode1 mode2]]
-  (let [param-1 (read-tape tape (inc pos) mode1)
-        param-2 (read-tape tape (+ pos 2) mode2)]
+(defn jmp-if-false [tape pos [mode1 mode2] rel-base]
+  (let [param-1 (read-tape tape (inc pos) mode1 rel-base)
+        param-2 (read-tape tape (+ pos 2) mode2 rel-base)]
     (if (= param-1 0)
-      [(- param-2 pos) tape]
-      [3 tape])))
+      [{:jump (- param-2 pos)} tape]
+      [{:jump 3} tape])))
 
-(defn jmp-if-true [tape pos [mode1 mode2]]
-  (let [param-1 (read-tape tape (inc pos) mode1)
-        param-2 (read-tape tape (+ pos 2) mode2)]
+(defn jmp-if-true [tape pos [mode1 mode2] rel-base]
+  (let [param-1 (read-tape tape (inc pos) mode1 rel-base)
+        param-2 (read-tape tape (+ pos 2) mode2 rel-base)]
     (if (= param-1 0)
-      [3 tape]
-      [(- param-2 pos) tape])))
+      [{:jump 3} tape]
+      [{:jump (- param-2 pos)} tape])))
 
-(defn equals [tape pos [lmode rmode]]
-  (let [param-1 (read-tape tape (inc pos) lmode)
-        param-2 (read-tape tape (+ pos 2) rmode)
-        dest-position (nth tape (+ pos 3))]
+(defn equals [tape pos [lmode rmode wmode] rel-base]
+  (let [param-1 (read-tape tape (inc pos) lmode rel-base)
+        param-2 (read-tape tape (+ pos 2) rmode rel-base)
+        dest-position (dest-pos tape (+ pos 3) rel-base wmode)]
     (if (= param-1 param-2)
-      [4 (assoc tape dest-position 1)]
-      [4 (assoc tape dest-position 0)])))
+      [{:jump 4} (assoc tape dest-position 1)]
+      [{:jump 4} (assoc tape dest-position 0)])))
 
-(defn less-than [tape pos [lmode rmode]]
-  (let [param-1 (read-tape tape (inc pos) lmode)
-        param-2 (read-tape tape (+ pos 2) rmode)
-        dest-position (nth tape (+ pos 3))]
+(defn less-than [tape pos [lmode rmode wmode] rel-base]
+  (let [param-1 (read-tape tape (inc pos) lmode rel-base)
+        param-2 (read-tape tape (+ pos 2) rmode rel-base)
+        dest-position (dest-pos tape (+ pos 3) rel-base wmode)]
     (if (< param-1 param-2)
-      [4 (assoc tape dest-position 1)]
-      [4 (assoc tape dest-position 0)])))
+      [{:jump 4} (assoc tape dest-position 1)]
+      [{:jump 4} (assoc tape dest-position 0)])))
+
+(defn adj-base [tape pos [mode] rel-base]
+  (let [base (read-tape tape (inc pos) mode rel-base)]
+    [{:jump 2 :base (+ rel-base base)} tape]))
 
 (defn parse-opcode [opcode]
   (if (< opcode 100)
@@ -63,7 +74,8 @@
     (letfn [(mode-sym [mode]
               (case mode
                 "0" :position
-                "1" :immediate))]
+                "1" :immediate
+                "2" :relative))]
       {:opcode (rem opcode 10)
        :modes (->> (str/split (str (int (/ opcode 100))) #"")
                    reverse
@@ -71,26 +83,32 @@
 
 (defn intcode-async
   ([tape pos channels]
+   (intcode-async tape pos channels 0))
+  ([tape pos channels rel-base]
    (let [{:keys [opcode modes]} (parse-opcode (nth tape pos))]
      (case opcode
-       99 [tape pos channels]
-       (let [[jump new-tape] (case opcode
-                               1 (op + tape pos modes)
-                               2 (op * tape pos modes)
-                               3 (mov (read-input channels) tape pos)
-                               4 (send-output tape pos modes channels)
-                               5 (jmp-if-true tape pos modes)
-                               6 (jmp-if-false tape pos modes)
-                               7 (less-than tape pos modes)
-                               8 (equals tape pos modes))]
-         (intcode-async new-tape (+ pos jump) channels))))))
+       99 (do
+            (a/close! (:output channels))
+            [tape pos channels])
+       (let [[{:keys [jump base]} new-tape] (case opcode
+                               1 (op + tape pos modes rel-base)
+                               2 (op * tape pos modes rel-base)
+                               3 (mov (read-input channels) tape pos modes rel-base)
+                               4 (send-output tape pos modes channels rel-base)
+                               5 (jmp-if-true tape pos modes rel-base)
+                               6 (jmp-if-false tape pos modes rel-base)
+                               7 (less-than tape pos modes rel-base)
+                               8 (equals tape pos modes rel-base)
+                               9 (adj-base tape pos modes rel-base))]
+         (intcode-async new-tape (+ pos jump) channels (or base rel-base)))))))
 
 (defn setup
   ([program]
-   (let [channels {:input (a/chan 10) :output (a/chan 10)}]
+   (let [channels {:input (a/chan 100) :output (a/chan 100000)}]
      (setup program channels)))
   ([program channels]
-    [channels (agent [program 0 channels])]))
+   (let [padded-program (into [] (concat program (take 100000 (repeat 0))))]
+     [channels (agent [padded-program 0 channels])])))
 
 (defn exec [a]
   (send-off a #(apply intcode-async %)))
