5b6ef98bb7b40df3ff469579976246b6593a30cf
Timothy Andrew <mail@timothyandrew.net>
2019-12-20 01:34:15 +0530

    day 16-2

--- src/advent_of_code_2019/day_16.clj
+++ src/advent_of_code_2019/day_16.clj
@@ -15,9 +15,10 @@
 (def example-2 (apply concat (repeat 2 example-1)))
 (def input (parse-input "59790132880344516900093091154955597199863490073342910249565395038806135885706290664499164028251508292041959926849162473699550018653393834944216172810195882161876866188294352485183178740261279280213486011018791012560046012995409807741782162189252951939029564062935408459914894373210511494699108265315264830173403743547300700976944780004513514866386570658448247527151658945604790687693036691590606045331434271899594734825392560698221510565391059565109571638751133487824774572142934078485772422422132834305704887084146829228294925039109858598295988853017494057928948890390543290199918610303090142501490713145935617325806587528883833726972378426243439037"))
 (defonce input-2 (apply concat (repeat 10000 input)))
+(def input-2-offset (Integer/parseInt (str/join (take 7 input-2))))
 
 (defn preserve-ones-place [n]
-  (last (str/split (str n) #"")))
+  (Integer/parseInt (last (str/split (str n) #""))))
 
 (defn repeating-pattern [index]
   (let [base [0 1 0 -1]
@@ -39,5 +40,19 @@
         (recur (mapint next)
                (inc i))))))
 
-(defn day-16-2 []
-  )
+(defn exec-fft-step [input total]
+  (loop [[digit & rest] input total total answer []]
+    (if digit
+      (recur
+       rest
+       (- total digit)
+       (conj answer (preserve-ones-place total)))
+      answer)))
+
+(defn fft-offset [inp phases offset]
+  (loop [i 0 input (drop offset inp) total (reduce + input)]
+    (println "PHASE " i)
+    (if (= i 100)
+      (take 8 input)
+      (let [next (exec-fft-step input total)]
+        (recur (inc i) next (reduce + next))))))
